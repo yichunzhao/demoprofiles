5826b24f07934cafe2421dacd5740f99692d7206
yichunzhao <zhaoyichun@hotmail.com>
2020-05-15 16:27:28 +0200

    Initial commit

---
+++ src/main/java/com/ynz/demoprofiles/ProfilesDemoApplication.java
@@ -0,0 +1,13 @@
+package com.ynz.demoprofiles;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProfilesDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProfilesDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ynz/demoprofiles/DemoprofilesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ynz.demoprofiles;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoprofilesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
